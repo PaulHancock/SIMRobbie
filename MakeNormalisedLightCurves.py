c2938590142d18e1908b0118106fef4eac9a26f9
PaulHancock <paul.hancock@curtin.edu.au>
2018-11-14 16:36:05 +0800

    move transient light curve into this module

--- MakeNormalisedLightCurves.py
+++ MakeNormalisedLightCurves.py
@@ -37,7 +37,23 @@ def get_lc(nepochs, flux, m):
     return get_norm_lc(nepochs=nepochs, m=m) * flux
 
 
+def get_transient_lc(nepochs, flux, transient_epoch):
+    """
+    Generate a light curve for a transient of given flux in a single epoch
+    :param nepohcs: Number of data points
+    :param flux: Transient flux
+    :param transient_epoch: The epoch in which the transient occurs (0 indexed)
+    :return:
+    """
+    lc = np.zeros(shape=nepochs)
+    lc[transient_epoch] = flux
+    return lc
+
+
 if __name__ == "__main__":
     lc = get_lc(nepochs=15, flux=0.75, m=0.2)
     print(lc)
     print("mean {0}, std {1}, m {2}".format(np.mean(lc), np.std(lc), np.std(lc)/np.mean(lc)))
+
+    t = get_transient_lc(nepochs=4, flux=0.62, transient_epoch=2)
+    print(t)
