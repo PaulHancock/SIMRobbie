0835ca472cbb7d59e84781908801b0b90d8e34ca
PaulHancock <paul.hancock@curtin.edu.au>
2018-11-19 11:00:32 +0800

    update sky area to better fit images

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ __author__ = 'Paul Hancock'
 __date__ = ''
 
 nepochs = 25
-nsrc = 10
-rarange = (175, 185)
-decrange = (-5, 5)
+nsrc = 1000
+rarange = (173, 187)
+decrange = (-7, 7)
 fluxrange = (1e-3, 1)
