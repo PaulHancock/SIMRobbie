16c6f610611aafa25ab9aefbe1c0ed8152dc8025
Paul Hancock <Paul.Hancock@curtin.edu.au>
2019-11-11 09:08:31 +0800

    list(zip) for python3

--- MakeRefCatalogue.py
+++ MakeRefCatalogue.py
@@ -47,7 +47,7 @@ def get_sources(rarange, decrange, fluxrange, nsrc):
     """
     ra, dec = get_positions(rarange, decrange, nsrc)
     flux = get_fluxes(fluxrange[0], fluxrange[1], nsrc)
-    return np.array(zip(ra, dec, flux))
+    return np.array(list(zip(ra, dec, flux)))
 
 
 if __name__ == "__main__":
