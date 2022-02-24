a12ba083f1a7307ad068ed7ba24c86d699443294
PaulHancock <paul.hancock@curtin.edu.au>
2022-02-24 10:27:40 +0800

    use settings for making region file create data dir if needed

--- MakeEpochImages.sh
+++ MakeEpochImages.sh
@@ -1,9 +1,15 @@
 #! /usr/bin/env bash
 
 dir=`python -c 'from settings import data_dir; print(data_dir)'`
+mkdir -p ${dir}
+
+ramin=`python -c 'from settings import rarange; print(rarange[0])'`
+ramax=`python -c 'from settings import rarange; print(rarange[1])'`
+decmax=`python -c 'from settings import decrange; print(decrange[0])'`
+decmin=`python -c 'from settings import decrange; print(decrange[1])'`
 
 echo "Making region file"
-MIMAS +p 173 -7 187 -7 187 7 173 7 -o ${dir}/square.mim
+MIMAS +p ${ramin} ${decmin} ${ramax} ${decmin} ${ramax} ${decmax} ${ramin} ${decmax} -o ${dir}/square.mim
 MIMAS --mim2reg ${dir}/square.mim ${dir}/square.reg
 
 echo "Making reference images"
